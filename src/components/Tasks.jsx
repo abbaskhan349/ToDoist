93af235a5cacb123baf439e31eb402f1deb75c3a
Muhammad Abbas Khan <mabbas349@gmail.com>
2023-01-21 02:04:11 +0500

    major improvements

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -79,7 +79,8 @@ function TodoList() {
   const [todos, setTodos] = useState([]);
   const [currentTodo, setCurrentTodo] = useState({
     title: '',
-    description: ''
+    description: '',
+    category: ''
   });
 
   const handleSubmit = event => {
@@ -87,7 +88,8 @@ function TodoList() {
     setTodos([...todos, currentTodo]);
     setCurrentTodo({
       title: '',
-      description: ''
+      description: '',
+      category: ''
     });
   }
 
@@ -115,6 +117,13 @@ function TodoList() {
           value={currentTodo.description}
           onChange={handleChange}
         />
+        <input 
+          type="text"
+          name='category' 
+          placeholder='category'
+          value={currentTodo.category}
+          onChange={handleChange}
+        />
         <button type="submit">Add Todo</button>
       </form>
 
@@ -125,23 +134,21 @@ function TodoList() {
       </ul> */}
 
       <div className='mt-10 flex justify-between'>
-         <h2>Board</h2>
+         <h2 className='text-xl'>Board</h2>
          <h4>This week</h4>
       </div>
 
       <div className='mt-5 flex justify-between space-x-2'>
-        <div className='flex flex-col w-[300px] h-[600px] bg-slate-100 p-2 rounded-lg'>
-          <h2>To Do</h2>
+        <div className='flex flex-col w-[300px] h-[600px] bg-slate-100 p-5 rounded-lg'>
+          <h2 className='mb-5 font-bold text-lg'>To Do</h2>
           {todos.map((todo, index) => (
-                <div key={index} className="flex flex-col bg-white p-4 rounded-lg">
-                  <h1 className='font-bold text-lg'>{todo.title}</h1>
-                  <p>{todo.description}</p>
+                <div key={index} className="flex flex-col bg-white p-4 mb-4 rounded-lg">
+                  <h6 className='text-[9px]'>{todo.category}</h6>
+                  <h1 className='font-semibold text-lg'>{todo.title}</h1>
+                  <p className='text-xs'>{todo.description}</p>
                 </div>
           ))}
         </div>
-
-        
-        
         
         <div className='w-[300px] bg-slate-100 p-2 rounded-lg'>
           In progress
